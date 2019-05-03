9a93cc3b381007a196af4a3576cbd449afcb835d
azinoVo <azinovo22@gmail.com>
2019-05-03 14:46:06 -0500

    debug older styled-component, tab and card appear

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -8,45 +8,42 @@ import styled from 'styled-components';
 // `
 // `
 
-// const HeaderWrap = styled.div
-// `
-// display: flex;
-// justify-content: space-between;
-// align-items: center;
-// flex-direction: row;
-// background-color: #fff;
-// border-bottom: 1px solid lightgrey;
-// width: 100%;
-// height: 120px;
-// margin-top: 44px;
-// padding-bottom: 15px;
-// }
-// @media (min-width: 1280px) {
-// .header {
-//   width: 1280px;
-// }
-// `
+const HeaderWrap = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-direction: row;
+background-color: #fff;
+border-bottom: 1px solid lightgrey;
+width: 100%;
+height: 120px;
+margin-top: 44px;
+padding-bottom: 15px;
+}
+@media (min-width: 1280px) {
+.header {
+  width: 1280px;
+}
+`
 
-// const Date = styled.span
-// `
-// margin-left: 25px;
-// flex: 1;
-// `
+const Date = styled.span`
+margin-left: 25px;
+flex: 1;
+`
 
-// const Temp = styled.span
-// `
-// text-align: right;
-// margin-right: 25px;
-// flex: 1;
-// `
+const Temp = styled.span`
+text-align: right;
+margin-right: 25px;
+flex: 1;
+`
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+    <HeaderWrap>
+      <Date>SMARCH 32, 2018</Date>
       <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+      <Temp>98°</Temp>
+    </HeaderWrap>
   )
 }
 
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -17,68 +17,64 @@ const TopBarWrap = styled.div`
   height: 44px;
   background-color: #333;`
 
-// const Container = styled.div
-//   `
-//   width: 100%;
-//   display: flex;
-//   justify-content: none;
-//   align-items: none;
-//   flex-direction: row;
-//   color: #fff;
-//   letter-spacing: 1px;
-//   padding: 0 10px;
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
 
-// @media (min-width: 1280px) {
-// .top-bar .container {
-//   width: 1280px;
-// }}
-// `
+@media (min-width: 1280px) {
+.top-bar .container {
+  width: 1280px;
+}}
+`
 
-// const ContainerLeft = styled.div
-//   `
-// display: flex;
-// justify-content: none;
-// align-items: center;
-// flex-direction: row;
-// flex: 1;
-// font-size: 11px;
-// `
+const ContainerLeft = styled.div`
+display: flex;
+justify-content: none;
+align-items: center;
+flex-direction: row;
+flex: 1;
+font-size: 11px;
+`
 
-// const ContainerCenter = styled.div
-//   `
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   flex-direction: row;
-//   flex: 3;
-//   font-size: 9px;
-// `
+const ContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`
 
-// const ContainerRight = styled.div
-//   `
-//   display: flex;
-//   justify-content: flex-end;
-//   align-items: center;
-//   flex-direction: row;
-//   flex: 1;
-//   font-size: 11px;
-//   font-weight: bold;
-// `
+const ContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`
 
 const TopBar = () => {
   return (
     <TopBarWrap>
-      <div className="container">
-        <div className="container-left">
+      <Container>
+        <ContainerLeft>
           <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
+        </ContainerLeft>
+        <ContainerCenter>
           <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
+        </ContainerCenter>
+        <ContainerRight>
           <span>LOG IN</span>
-        </div>
-      </div>
+        </ContainerRight>
+      </Container>
     </TopBarWrap>
   )
 }
